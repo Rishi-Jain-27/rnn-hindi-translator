ffa4caa3babe52b2de39788cbf8fb11d1157a0f1
Rishi Jain <27rishijainpersonalemail@gmail.com>
2026-05-20 20:09:30 -0400

    Completed transformer.py

--- src/nmt/model/transformer.py
+++ src/nmt/model/transformer.py
@@ -9,4 +9,34 @@ decode_step(...) over a KV-cache. A build_model(ModelConfig, tokenizer) factory 
 vocab_size + special-token ids (pad/bos/eos/unk) into the model so they are set once.
 """
 
-# TODO(rishi): implement
+import torch
+import torch.nn as nn
+from ..config import ModelConfig
+from .encoder import Encoder
+from .decoder import Decoder
+
+class Transformer(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.encoder = Encoder(config)
+        self.decoder = Decoder(config)
+        if config.tie_embeddings:
+            self.decoder.embed = self.encoder.embed
+
+    def forward(self, src_ids, tgt_ids, src_pad_mask, tgt_pad_mask):
+        memory = self.encoder(src_ids, src_pad_mask)
+        logits = self.decoder(tgt_ids, memory, tgt_pad_mask, src_pad_mask)
+        return logits
+    
+    def encode(self, src_ids, src_pad_mask):
+        return self.encoder(src_ids, src_pad_mask)
+
+def build_model(config, tokenizer):
+    config.vocab_size = tokenizer.vocab_size
+    config.pad_id = tokenizer.pad_id
+    config.bos_id = tokenizer.bos_id
+    config.eos_id = tokenizer.eos_id
+    config.unk_id = tokenizer.unk_id
+
+    return Transformer(config)
+
